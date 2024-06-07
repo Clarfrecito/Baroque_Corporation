5de688261d45dc64e642eed9ac4f7831d4247521
Clarfrecito <164571554+Clarfrecito@users.noreply.github.com>
2024-06-07 10:34:14 -0300

    Update prueba.php

--- Prueba/prueba.php
+++ Prueba/prueba.php
@@ -1,2 +1,22 @@
-<?php
-    echo "Hola Mundo";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1>Hola Mundo en HTML</h1>
+        <?php
+        echo "¡Hola, mundo desde PHP!";
+        ?>
+    </main>
+    <footer>
+        <h3>Creado por Baroque Corporation</h3>
+    </footer>
+</body>
+</html>
