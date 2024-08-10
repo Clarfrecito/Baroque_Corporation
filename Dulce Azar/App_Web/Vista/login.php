e3ab1f1c8ac765b53ac8eb40aab3c7828528c145
Tomás Palumbo <tomi.palumbo07@gmail.com>
2024-08-10 19:15:27 -0300

    App_Web 1.0
    Falta Local/Visitante y Bingo Loco

---
+++ Dulce Azar/App_Web/Vista/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Inicio de Sesion</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="stylesL.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="../index.php">
+            <img src="../../DulceAzar.png" alt="Logo" >  
+        </a>
+    </header>
+    <main>
+        <form action="../Controlador/registrar.php" method="post">
+        <div class="Inicio de Sesion">
+            <h1>Inicio de Sesion</h2>
+            <input type="text" id="username" name="username" required placeholder="Usuario"><br><br>
+            <input type="password" id="password" name="password" required placeholder="Contraseña"><br><br>
+            <button type="submit" name="login">Iniciar Sesion</button>
+        </form>
+        </div>
+    </main>
+</body>
+</html>
